cf55e557c83f2e9c9d5acda570636b008dd478a9
m3h3d1 <hasanmehedi185@gmail.com>
2022-03-12 02:52:15 +0600

    reg files

--- login.php
+++ login.php
@@ -23,7 +23,7 @@
     else {
       $message = "Invalid Username or Password!";
     }
-    echo $_SESSION['name'];
+    // echo $_SESSION['name'];
   }
   // if(isset($_SESSION["email"])) {
   //   header("Location:index.php");
@@ -39,6 +39,14 @@
   <title>Login</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta charset="utf-8">
+
+
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+
   <link rel="stylesheet" type="text/css" href="login_style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>  
@@ -48,6 +56,83 @@
 </head>
 
 <body class="body">
+
+<!-- navigation starts -->
+<div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark dmenu">
+            <div class="container-fluid dmenudiv">
+              <!-- <a class="navbar-brand" href="https://www.police.gov.bd/"> -->
+                <img src="images/logo2.png">
+              </a>
+              <button class="navbar-toggler dtoggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                  </li>
+                  
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Area     
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a class="dropdown-item" href="#">Dhaka</a></li>
+                      <li><a class="dropdown-item" href="#">Chittagong</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="#">Other</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="Pages/about.php">About Us</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Contact</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="Pages/faq.php">FAQ</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <?php
+                      if($_SESSION["name"]) {
+                      ?>
+                      <?php echo '<p style="color: white;"> Welcome '.$_SESSION["name"].'</p>'; ?>. <a href="logout_session.php" tite="Logout">Logout.
+                      <?php
+                      }else {
+                        // echo "<p style=\"color:white;\">Please login first .</p>";
+                        echo '<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          Login
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                          <li><a class="dropdown-item" href="login.php">User</a></li>
+                          <li><a class="dropdown-item" href="official_login.php">Admin</a></li>
+                        </ul>';
+                      }
+
+                    ?>
+                    <!-- <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Login
+                    </a> -->
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a class="dropdown-item" href="login.php">User</a></li>
+                      <li><a class="dropdown-item" href="official_login.php">Admin</a></li>
+                    </ul>
+                  </li>
+                </ul>
+                <form class="d-flex">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+</div>
+
+<script src="Js/bootstrap.min.js"></script>
+<script src="Js/jquery-3.6.0.min.js"></script>
+<!-- navigation ends -->
+
 	
 <div class="login-page">
   <div class="form">
@@ -60,7 +145,7 @@
       <i class="fas fa-eye" onclick="show()"></i> 
       <br>
       <br>
-      <button type="submit" name="login">LOGIN</button>
+      <button type="submit" name="login">Login</button>
       <p class="message"></p>
     </form>
     <?php
@@ -71,7 +156,7 @@
     ?>
 
     <form class="login-form">
-      <button type="button" onclick="window.location.href='signup.php'">SIGN UP</button>
+      <button type="button" onclick="window.location.href='registration.php'">Register</button>
     </form>
   </div>
 </div>
