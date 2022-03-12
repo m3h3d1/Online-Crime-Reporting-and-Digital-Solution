d5cbb0b57848b80f566587e67a101bc718a5095f
H-Alexa <54031572+H-Alexa@users.noreply.github.com>
2022-03-12 17:30:46 +0600

    Added history page

---
+++ Pages/history.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+	
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/responsive.css">
+    
+
+    <title>History</title>
+</head>
+<body>
+
+    <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark dmenu">
+            <div class="container-fluid dmenudiv">
+              <a class="navbar-brand" href="https://www.police.gov.bd/">
+                <img src="../images/logo2.png">
+              </a>
+              <button class="navbar-toggler dtoggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Area     
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a class="dropdown-item" href="#">Dhaka</a></li>
+                      <li><a class="dropdown-item" href="#">Chittagong</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="#">Other</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="about.php">About Us</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="./faq.php">FAQ</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Logout</a>
+                  
+                </ul>
+                <form class="d-flex">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+          <div class="container">
+            <div class="card mb-3">
+                <img src="../images/crime1.jpg" class="card-img-top crimag" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Regular GD</h5>
+                    <p class="card-text">
+                        
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum velit in purus consequat rutrum. Fusce a porttitor nibh. Maecenas elementum elementum neque, sit amet rutrum ipsum rutrum auctor. Donec tempus justo est, vel consequat sem consectetur vitae. Sed dui odio, venenatis eu dignissim non, vulputate sed diam. Nullam risus lacus, lacinia eu tristique ut, tincidunt vitae metus. Suspendisse tempor molestie dui, sed lobortis quam aliquam a. In erat magna, feugiat a malesuada et, varius ut magna. Donec dignissim augue ex, sed aliquam diam pretium eget. Nulla dignissim semper felis, eget accumsan sem mollis ut. Praesent vulputate libero et purus pulvinar, at mattis tortor tincidunt. In hac habitasse platea dictumst.
+                    </p>
+                    <a href="#" class="btn btn-primary">See more</a>
+                    <a href="#" class="btn btn-secondary">Download</a>
+                    
+                    <p class="card-text" style="text-align: right;"><small class="text-muted">January 3, 2022</small></p>
+                </div>
+                </div>
+                <div class="card mb-3">
+                <img src="../images/crime1.png" class="card-img-top crimag" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Emergency GD</h5>
+                    <p class="card-text">
+                        
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum velit in purus consequat rutrum. Fusce a porttitor nibh. Maecenas elementum elementum neque, sit amet rutrum ipsum rutrum auctor. Donec tempus justo est, vel consequat sem consectetur vitae. Sed dui odio, venenatis eu dignissim non, vulputate sed diam. Nullam risus lacus, lacinia eu tristique ut, tincidunt vitae metus. Suspendisse tempor molestie dui, sed lobortis quam aliquam a. In erat magna, feugiat a malesuada et, varius ut magna. Donec dignissim augue ex, sed aliquam diam pretium eget. Nulla dignissim semper felis, eget accumsan sem mollis ut. Praesent vulputate libero et purus pulvinar, at mattis tortor tincidunt. In hac habitasse platea dictumst.
+                    </p>
+                    <a href="#" class="btn btn-primary">See more</a>
+                    <a href="#" class="btn btn-secondary">Download</a>
+                    <p class="card-text" style="text-align: right;"><small class="text-muted">January 3, 2022</small></p>
+                </div>
+                </div>
+                
+            
+            </div>
+          </div>
+
+
+    </div>
+
+    <script src="../Js/bootstrap.min.js"></script>
+    <script src="../Js/jquery-3.6.0.min.js"></script>
+</body>
+</html>
