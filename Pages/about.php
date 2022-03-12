b2a39dfb982264c8abe5a3734e685dc382bc4362
Shahriar Himel <himelit47@gmail.com>
2022-03-12 22:25:03 +0600

    about page

--- Pages/about.php
+++ Pages/about.php
@@ -13,6 +13,7 @@
     if(mysqli_num_rows($res)>0){
       $_SESSION['name']=$row['name'];    
       $_SESSION['email'] = $row['email'];
+      $_SESSION['nid'] = $row['nid'];
       echo $_SESSION['name'];
       header('location:../index.php');
     }
@@ -26,32 +27,32 @@
   // }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+
+
+<!DOCTYPE HTML>
+<html lang="en" >
+<html>
 <head>
-<title>Anout Us</title>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+  <title>About Us</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
 
 
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500&family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/responsive.css">
-
+    <link rel="stylesheet" href="../css/main.css">
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>  
   <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'> 
   <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
-
 </head>
+
 <body>
-    
 
 <!-- navigation starts -->
 <div class="container-fluid">
@@ -66,7 +67,7 @@
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                   <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+                    <a class="nav-link" aria-current="page" href="../index.php">Home</a>
                   </li>
                   
                   <li class="nav-item dropdown">
@@ -81,10 +82,10 @@
                     </ul>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="about.php">About Us</a>
+                    <a class="nav-link active" href="about.php">About Us</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Contact</a>
+                    <a class="nav-link" aria-current="page" href="contact.php">Contact</a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="faq.php">FAQ</a>
@@ -125,11 +126,190 @@
           </nav>
 </div>
 
+
+<div class="col-md-12 c-head">
+            <h1>About Us</h1>
+            <h2>BD Police</h2>
+            <div class="col-md-6 a-data">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Mission</h4>
+                        <p class="card-text">Bangladesh Police is committed to enforce law, maintain social order, reduce fear of crime,enhance public safety and ensure internal security with the active support of the community.</p>
+                     </div>
+                </div>
+            </div>
+            <div class="col-md-6 a-data">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Vision</h4>
+                        <p class="card-text">To provide quality service by competent, efficient and dedicated professionals enjoying trust and respect of citizens to make Bangladesh a better and safer place to live.</div>
+                </div>
+            </div>
+            </div>
+            <div class="clr"></div>
+    <div class="col-md-12 act">
+                      <h3>Our Activities</h3>
+                      <p>Bangladesh Police devotes a round the clock service to public order, peace and security. It works relentlessly to secure life and property of the people. The enormous area of responsibility and the volume of works of Bangladesh Police entail activities ranging from domestic law and order management to performing globally with the UN in its different peacekeeping missions. The functions in generally carried out by Bangladesh Police are as follows:</p>
+                <div class="col-md-3 ac-data">
+                    <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Crime Management</h4>
+                        <ul>
+                            <li>Collecting intelligence</li>
+                            <li>Response to crime Incidents</li>
+                            <li>Investigation</li>
+                            <li>Verification</li>
+                            <li>Public Order Management</li>
+                            <li>Traffic Management</li>
+                            <li>Assisting Prosecution</li>
+                        </ul>
+                     </div>
+                </div>  
+            </div>
+
+            <div class="col-md-3 ac-data">
+                    <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Internal Security</h4>
+                        <ul>
+                            <li>Security Patrols</li>
+                            <li>Security Watchdog</li>
+                            <li>VVIP Security</li>
+                            <li>KPI Security</li>
+                            <li>Security at National Occasions (Religious festival, Fair, Ijtema, Pahela Baishakh etc.)</li>
+                    
+                        </ul>
+                     </div>
+                </div>  
+            </div>
+
+            <div class="col-md-3 ac-data">
+                    <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Social Integration</h4>
+                        <ul>
+                            <li>Raising Awareness (Through Training, Rally, Exhibition, Media Coverage, Visiting schools etc.)</li>
+                            <li>Community Policing</li>
+                            <li>Humanitarian Efforts (Winter cloth distribution, helping disaster victims etc)</li>
+                            <li>Participation in the Social Events (Being partner in Events like fair, assistance etc.)</li>
+                            <li>Observing Open House Day</li>
+                            <li>Blood Donation</li>
+                            <li>Victim Support Center</li>
+                        </ul>
+                     </div>
+                </div>  
+            </div>
+
+            <div class="col-md-3 ac-data">
+                    <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Performing Internationally</h4>
+                        <ul>
+                            <li>Addressing Transnational Crimes (Interpol, SAARCPol etc)</li>
+                            <li>UN Peacekeeping Missions</li>
+                        </ul>
+                     </div>
+                </div>  
+            </div>
+    </div>
+    <div class="clr"></div>
+
+    <div class="col-md-12 history">
+        <h3>History</h3>
+        <div class="col-md-4 himage">
+                    <div class="card">
+                     <img src="../Images/h1.jpg" alt="h1">
+                </div>  
+        </div>
+        <div class="col-md-4 himage">
+                    <div class="card">
+                     <img src="../Images/h2.jpg" alt="h1">
+                </div>  
+        </div>
+        <div class="col-md-4 himage">
+                    <div class="card">
+                     <img src="../Images/h3.jpg" alt="h1">
+                </div>  
+        </div>
+        <div class="clr"></div>
+        <div class="col-md-12">
+        <p>Bangladesh Police is the core law enforcement agency of Bangladesh. It is administered under the Ministry of Home Affairs of the Government of Bangladesh. It plays the prime and pivotal role in maintaining law and order of the state. Though police is primarily concerned with maintaining law and order assuring security of public life and property. In Bangladesh, the formal and organized policing with varieties of activities as of today has come to this stage through lots of evolutions and developments across the long time......</p>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
+    Details
+  </button>
+
+
+  <div class="modal" id="myModal">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">History</h4>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+      Chronology :
+
+Bangladesh Police is the core law enforcement agency of Bangladesh. It is administered under the Ministry of Home Affairs of the Government of Bangladesh. It plays the prime and pivotal role in maintaining law and order of the state. Though police is primarily concerned with maintaining law and order assuring security of public life and property.
+
+In Bangladesh, the formal and organized policing with varieties of activities as of today has come to this stage through lots of evolutions and developments across the long time.
+
+Ancient Period:
+
+History unfolds the fact that police is as old as the civilization. Towards the Middle of the first century B.C. police emerged as a special institution in ancient Rome in the regime of Augustus.Manushanghita, the hieroglyphics of Emperor Ashoka, and the stories of renowned travelers are the main sources of our history. These sources also give clues to compose the fragmented history of Bangladesh Police. In Orthoshastra by Koutilla, nine types of spies are mentioned. During that period policing was confined to the efforts of collecting intelligence in order to curb anti-government activities and to maintain law and order in the society. The duties of undercover spies were extended such a way that they used to conduct surveillance over the activities of ministers, civil and military officials as well. In the process, all tangible means of temptations and instigations were deployed. Information about investigating techniques and investigating authorities may be found in Orthoshastra. The procedures of punishing the accused are also found in this book. Hence, it may be assumed that there happened to be a kind of police under the local autonomous system in the rural and urban areas.
+
+British Period:
+
+The establishment of a systaematic police force in England was of slow growth and came into effect after its creation abroad. In the early stage of Industrial Revolution when England was facing grave crisis due to socioeconomic transformation, the necessity of an effective organized police service was keenly felt. Sir Robert Peel, the then Prime Minister introduced a bill in the British Parliament in 1829 which created an organized civil police in the nearby London Metropolis. Initially there were some opposition to the existence of this uniform police but the success of the London police in controlling social disorder and crime captured imagination of not only the people of England but also of the whole Europe and America. New York city copied the London model with some modifications when it organized the first Municipal Police Force 1833.
+
+In 1858 full control of the Indian Territory was taken over from the East India Company by the British government. The success of the London police organized under Peels Act of 1829, prompted the British government to reform the police system in the sub-continent on the line of the British constabularies. With this end in view, a police commission was set up in 1840 and on the recommendation of the commission, the Police Act (Act V of 1861) was passed by the British Parliament. Under this Act a police force was created in each province of British India and placed under the control of the provincial government. The administration of the police force of a province was vested upon an officer styled as the Inspector General of Police. The administration of the police in a district was placed under the Superintendent of Police. The Act is still in force throughout the Sub-continent and has been regulating the function of police in Bangladesh as well as in other countries of the sub-continent.
+
+The history and heritage of Bangladesh Police is marked by the blend of the traits of colonial- imperial rule and the system of internal security of a feudal society. The philosophy of police of the British regime had never been complementary to democratic values and political development. The British who ruled this country had a twisted idea of using police as an instrument of coercion for their own interest by subtly branding it as “myrmidon of law”. The police system introduced during that period was governed more by consideration of maintaining control of dictatorial rule rather than providing service to the people. The Police Act, 1861 enabled to form a well-organized and well-structured police force only to serve the interest of the colonial masters.
+
+Pakistan Period:
+
+After the partition of the Sub-continent in 1947, police force in Bangladesh was first named as East Bengal Police and then as East Pakistan Police and it continued to function as provincial police force in the same lines as it was during the British rule.
+
+However, the police force of Pakistan continued the system of British period. Police were compelled to carry out unpopular orders. The act of shooting on the participants of language movement demonstration in 1952 was a glaring example of colonial rule and suppression.
+
+Role of Police in Liberation War:
+
+The most glorious chapter of the history of Bangladesh Police is that Bangla speaking members of our police participated along with the citizens in our Independence War. The resistance by the Bengali members of police at Rajarbag is basically the first chapter of armed struggles during our War of independence. Many police personnel embraced martyrdom on 25 March 1971 fighting bravely with mere .303 rifles against the Pakistani invaders.
+During the liberation war a large number of police officers from all ranks including a Deputy Inspector General, some senior Superintendents of Police and many other gave their lives for the cause of liberation. The name and address of 1262 police officers of different ranks could be listed who sacrificed their lives for the independence of Bangladesh during the liberation war.
+What is more to be mentioned with pride, Mr. Mahbubuddin Ahmed, Bir Bikram, who was the Sub-Divisional Police Officer (SDPO) of Jhenaidah at that time, led the historic guard of honors given to the members of the Mujibnagar Cabinet when the provisional Government of Bangladesh took oath on the auspicious day of April 17, 1971 during liberation war. All these go to show that our police has been inseparably bound up with the umbilical cord with their motherland and fellow countrymen.
+
+Bangladesh Period:
+
+After the emergence of Bangladesh as an independent country on 16 December 1971, the existing police force of Bangladesh got remodeled and reshaped as Bangladesh Police and assumed the role of a national police force. Bangladesh Police is primarily responsible for the preservation of peace and order, protection of life and property of the people and prevention and detection of crime within the periphery of the state. The traditional role of police in Bangladesh has undergone significant changes after the liberation. The role of police, at the moment, is no longer confined to maintenance of law and order and prevention and detection of crime. Apart from the mandatory works that needs to be done, there are manifold problems that need taking care of. To meet the need of an independent and developing country, the police is now required to play a significant role in the development process by providing the basic security required for sustained economic growth of the country. Police also is contributing substantially in this field by keeping under control the economic and techno-crimes which retard the process of the development. It is further playing a vital role in disaster management, environment, ecotourism, bio-diversity conservation and so on, which have got enormous impact on state economy and development.
+
+Bangladesh police, the single most institution entrusted with the responsibility of ensuring rule of law and human rights, is at a cross-road to promote itself to a height to adopt the role of service rather than force, devoted to providing reassurance of service delivery, flexibility to community wishes, cares and social justice.
+
+It strives to track citizen’s engagement in policing and theiaczr quest for better service, better value and better management. It envisages a new structure erected on the bedrock of information and communication technology, skill adequacy, knowledge-based initiatives and community partnership that will be styled on a schema of cascading change to set a stage for a clear interface between police and citizenry.
+
+It tends to explore innovative practices in addressing the challenges of the time and reorienting its mission and vision and augment its capacity towards citizen centric service, to uphold professionalism, image and visibility of the service and the value and virtue of the service to the community.
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div>
+        </div>
+    </div>
+
+
 <script src="../Js/bootstrap.min.js"></script>
 <script src="../Js/jquery-3.6.0.min.js"></script>
 <!-- navigation ends -->
 
+	
 
-
+   
 </body>
 </html>
