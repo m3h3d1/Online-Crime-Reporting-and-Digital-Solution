5c9a2e8ea62e443b8582465281283dee59ae1acb
m3h3d1 <hasanmehedi185@gmail.com>
2022-03-12 23:22:30 +0600

    Revert "navbar, footer"
    This reverts commit 65b760ae568cc875b60677acd8e3c13b9a3b2f3f.

--- Pages/login.php
+++ Pages/login.php
@@ -1,4 +1,3 @@
-@@ -0,0 +1,106 @@
 <?php 
   session_start();
   $message="";
@@ -55,9 +54,81 @@
 
 <body class="body">
 
-<?php
-include "../php/navbar.php";
-?>
+<!-- navigation starts -->
+<div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark dmenu">
+            <div class="container-fluid dmenudiv">
+              <!-- <a class="navbar-brand" href="https://www.police.gov.bd/"> -->
+                <img src="../images/logo2.png">
+              </a>
+              <button class="navbar-toggler dtoggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="../index.php">Home</a>
+                  </li>
+                  
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Area     
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a class="dropdown-item" href="#">Dhaka</a></li>
+                      <li><a class="dropdown-item" href="#">Chittagong</a></li>
+                      <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="#">Other</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="about.php">About Us</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Contact</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="faq.php">FAQ</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <?php
+                      if($_SESSION["name"]) {
+                      ?>
+                      <?php echo '<p style="color: white;"> Welcome '.$_SESSION["name"].'</p>'; ?>. <a href="logout_session.php" tite="Logout">Logout.
+                      <?php
+                      }else {
+                        // echo "<p style=\"color:white;\">Please login first .</p>";
+                        echo '<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          Login
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                          <li><a class="dropdown-item" href="login.php">User</a></li>
+                          <li><a class="dropdown-item" href="official_login.php">Admin</a></li>
+                        </ul>';
+                      }
+
+                    ?>
+                    <!-- <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Login
+                    </a> -->
+                    <!-- <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a class="dropdown-item" href="login.php">User</a></li>
+                      <li><a class="dropdown-item" href="official_login.php">Admin</a></li>
+                    </ul> -->
+                  </li>
+                </ul>
+                <form class="d-flex">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+</div>
+
+<script src="../Js/bootstrap.min.js"></script>
+<script src="../Js/jquery-3.6.0.min.js"></script>
+<!-- navigation ends -->
 
 	
 <div class="login-page">
